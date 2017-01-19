34ae1327a524768985b6da27219c571286090122
stanislavangelkov <hkraziel@gmail.com>
2017-01-19 09:23:13 +0200

    Data Types and Variables Exercises

---
+++ 01. Data Types and Variables - Lab/01. Data Types and Variables - Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesAndVariables_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Centuries = ");
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+            Console.WriteLine($"{centuries} Centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
---
+++ 01. Data Types and Variables - Lab/05. SpecialNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            bool special;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 10 + i / 10 == 5 || i % 10 + i / 10 == 7 || i % 10 + i / 10 == 11)
+                {
+                    special = true;
+                }
+                else
+                {
+                    special = false;
+                }
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
