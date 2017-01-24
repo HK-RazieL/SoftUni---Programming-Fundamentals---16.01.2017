72737422ef55c7ccc2fbf74c9bcac70afaeb0f7e
stanislavangelkov <hkraziel@gmail.com>
2017-01-24 09:45:13 +0200

    Exercises

---
+++ 03. Methods and Debugging - Lab - 23.01.2017/08. MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _08.MultiplyEvensByOdds
+{
+    using System;
+
+    public class MultiplyEvensByOdds
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(TotalSumOfEvenAndOdd(Math.Abs(number)));
+        }
+
+        public static int TotalSumOfEvenAndOdd(int number)
+        {
+            int sumOfEven = GetSumOfEvenDigits(number);
+            int sumOfOdd = GetSumOfOddDigits(number);
+                
+            return sumOfEven * sumOfOdd;
+        }
+
+        public static int GetSumOfEvenDigits(int number)
+        {
+            int even = 0;
+
+            while (number > 0)
+            {
+                int lastNumber = number % 10;
+                if (lastNumber % 2 == 0)
+                {
+                    even += lastNumber;
+                }
+
+                number /= 10;
+            }
+
+            return even;
+        }
+
+        public static int GetSumOfOddDigits(int number)
+        {
+            var odd = 0;
+
+            while (number > 0)
+            {
+                int lastNumber = number % 10;
+                if (lastNumber % 2 == 1)
+                {
+                    odd += lastNumber;
+                }
+
+                number /= 10;
+            }
+
+            return odd;
+        }
+    }
+}
