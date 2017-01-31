019d264824d855287f19c70a6eab91919098e193
stanislavangelkov <hkraziel@gmail.com>
2017-01-31 16:23:34 +0200

    Exercises

---
+++ 07. Lists - Lab/03. SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.SumAdjacentEqualNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAdjacentEqualNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                while (i < numbers.Count - 1)
+                {
+                    if (numbers[i] == numbers[i + 1])
+                    {
+                        numbers[i] = numbers[i] + numbers[i + 1];
+                        numbers.RemoveAt(i + 1);
+                        if (i > 0)
+                        {
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
