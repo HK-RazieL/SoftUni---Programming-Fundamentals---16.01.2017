8eafe3396cfeb149902d4e1eff483fe591078905
stanislavangelkov <hkraziel@gmail.com>
2017-02-01 15:34:50 +0200

    Exercises

---
+++ 08. Lists - Exercises 31.01.2017/01. MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.MaxSequenceOfEqualElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MaxSequenceOfEqualElements
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var currentSequence = new List<int>();
+            var longestSequence = new List<int>();
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentSequence.Add(numbers[i - 1]);
+                }
+               
+
+               
+            }
+        }
+    }
+}
---
+++ 09. Dictionaries, Lambda and LINQ - Lab - 01.02.2017/01. CountRealNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
