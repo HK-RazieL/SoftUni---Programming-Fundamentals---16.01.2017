9c74617a042d310691c93b8727684634452b6d38
stanislavangelkov <hkraziel@gmail.com>
2017-02-01 13:21:25 +0200

    Exercises

---
+++ 06. Lists - Lab - 30.01.2017/01. Lists - Lab/01. Lists - Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.Lists___Lab
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class RemoveNegativesandReverse
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var item in list)
+            {
+                Console.Write(list);
+            }
+        }
+    }
+}
