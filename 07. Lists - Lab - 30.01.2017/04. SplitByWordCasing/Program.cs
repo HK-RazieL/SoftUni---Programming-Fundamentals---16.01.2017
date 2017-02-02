7285dc40a601de2e1d7eebebfde426418fc5bb49
stanislavangelkov <hkraziel@gmail.com>
2017-02-02 09:59:34 +0200

    Exercises

---
+++ 07. Lists - Lab - 30.01.2017/04. SplitByWordCasing/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _04.SplitByWordCasing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SplitByWordCasing
+    {
+        public static void Main()
+        {
+            var separator = new char[] { ' ', ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']' };
+            var firstString = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var lowerCase = new List<string>();
+            var mixedCase = new List<string>();
+            var upperCase = new List<string>();
+
+            foreach (var word in firstString)
+            {
+                if (word.All(char.IsUpper))
+                {
+                    upperCase.Add(word);
+                }
+                else if (word.All(char.IsLower))
+                {
+                    lowerCase.Add(word);
+                }
+                else
+                {
+                    mixedCase.Add(word);
+                }
+            }
+
+            Console.WriteLine($"Lower-case: {string.Join(", ", lowerCase)}");
+            Console.WriteLine($"Mixed-case: {string.Join(", ", mixedCase)}");
+            Console.WriteLine($"Upper-case: {string.Join(", ", upperCase)}");
+        }
+    }
+}
---
+++ 07. Lists - Lab - 30.01.2017/06. SquareNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.SquareNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SquareNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var newNumbers = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                if (Math.Sqrt(item) == (int)Math.Sqrt(item))
+                {
+                    newNumbers.Add(item);
+                }
+            }
+
+            newNumbers.Sort();
+            newNumbers.Reverse();
+
+            Console.WriteLine(string.Join(" ", newNumbers));
+        }
+    }
+}
