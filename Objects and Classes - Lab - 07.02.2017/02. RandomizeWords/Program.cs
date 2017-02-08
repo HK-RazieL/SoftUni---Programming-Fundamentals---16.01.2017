29020460a50cafa2ce4f8ad9d6a16a5b0d77700f
stanislavangelkov <hkraziel@gmail.com>
2017-02-08 21:59:47 +0200

    Exercises

---
+++ Objects and Classes - Lab - 07.02.2017/02. RandomizeWords/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.RandomizeWords
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class RandomizeWords
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().Split(' ');
+            var rng = new Random();
+            var result = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var temp = text[i];
+                var index = rng.Next(0, text.Length);
+                var randomWord = text[index];
+                text[i] = randomWord;
+                text[index] = temp;
+            }
+
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Objects and Classes - Lab - 07.02.2017/04. DistanceBetweenPoints/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04.DistanceBetweenPoints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DistanceBetweenPoints
+    {
+        public static void Main()
+        {
+            var pointOne = Console.ReadLine().Split().Select(double.Parse).ToList();
+            var pointTwo = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            var firstPoint = new Point();
+            firstPoint.X = pointOne[0];
+            firstPoint.Y = pointOne[1];
+
+            var secondPoint = new Point();
+            secondPoint.X = pointTwo[0];
+            secondPoint.Y = pointTwo[1];
+
+            Console.WriteLine($"{calculating(firstPoint, secondPoint):F3}");
+        }
+
+        public static double calculating(Point firstPoint, Point secondPoint)
+        {
+            var sideA = firstPoint.X - secondPoint.X;
+            var sideB = firstPoint.Y - secondPoint.Y;
+            var distance = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+            return distance;
+        }
+    }
+}
